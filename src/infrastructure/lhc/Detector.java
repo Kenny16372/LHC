e5342a243c1ed2bfbd5dd4a6dc584436e68b0356
Kenny16372 <44952455+Kenny16372@users.noreply.github.com>
2020-02-01 16:28:11 +0100

    kleinere Änderungen; finde es cool, dass ich jetzt wieder Deutsch schreibe

--- src/infrastructure/lhc/Detector.java
+++ src/infrastructure/lhc/Detector.java
@@ -1,19 +1,17 @@
 package infrastructure.lhc;
 
 import java.util.LinkedList;
-
 import  infrastructure.security.Reader;
 
 import com.google.common.eventbus.Subscribe;
 import infrastructure.lhc.events.Analyse;
-import lhc.Main;
-
 import java.util.LinkedList;
+import  infrastructure.security.reader.Reader;
 
 public class Detector extends Subscriber{
     private String higgsBosonStructure;
     private boolean isActivated;
-    private LinkedList<Experiment> experimentList = new LinkedList<>();
+    private LinkedList<Experiment> experimentList;
     private Reader reader;
 
     public Experiment getCurrentExperiment(){
@@ -26,20 +24,10 @@ public class Detector extends Subscriber{
     
     @Subscribe
     public void receive(Analyse event){
-        Experiment experiment = event.experiment;
-        boolean found = false;
-        this.experimentList.add(experiment);
-        for(Block b:experiment.getBlocks()){
-            try{
-                if(Main.search(b.getStructure(), this.higgsBosonStructure) > 0){
-                    found = true;
-                }
-            }catch(Exception e){
-                e.printStackTrace();
-            }
+        
+    }
 
-        }
-        experiment.setIsHiggsBosonFound(found);
-        System.out.println(experiment.getUUID() + ": " + experiment.getIsHiggsBosonFound());
+    public void addExperiment(Experiment e){
+        this.experimentList.add(e);
     }
 }
