4a4f2a75a5557a385140a8371a2d6be2fab1f9d0
Kenny16372 <44952455+Kenny16372@users.noreply.github.com>
2020-02-01 16:11:38 +0100

    Works

--- src/infrastructure/lhc/Ring.java
+++ src/infrastructure/lhc/Ring.java
@@ -4,10 +4,8 @@ import com.google.common.eventbus.Subscribe;
 import infrastructure.lhc.events.RunExperimentFull;
 import infrastructure.lhc.events.RunExperimentPartial;
 import infrastructure.security.ControlCenter;
-import infrastructure.security.ExperimentScope;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.UUID;
 
 public class Ring extends Subscriber{
@@ -42,42 +40,18 @@ public class Ring extends Subscriber{
             trap.release();
         }
     }
-
-    public void generateProtons(ExperimentScope scope){
-        int counter = 0;
-        switch(scope){
-            case ES5:
-                counter = 10;
-                break;
-            case ES10:
-                counter = 20;
-                break;
-            case ES20:
-                counter = 40;
-                break;
-            case ESFull:
-                counter = 50;
-        }
-        this.protonTraps = new ProtonTrap[counter];
-        for(int i=0; i< counter; i++){
-            this.protonTraps[i] = new ProtonTrap();
-            this.protonTraps[i].loadData("data/proton_" + String.format("%02d", i+1) + ".txt");
-            System.out.println("Loaded file: data/proton_" + String.format("%02d", i+1) + ".txt");
-        }
-
-    }
     public void increaseEnergy(int delta){}
     public void collide(Proton proton01, Proton proton02 ){
         ArrayList<Block> blocks = new ArrayList<Block>();
-        char[][][] structure1 = proton01.getStructure();
-        char[][][] structure2 = proton02.getStructure();
+        int[][][] structure1 = proton01.getStructure();
+        int[][][] structure2 = proton02.getStructure();
         for(int x=0; x < 100; x++){
             for(int y=0; y < 100; y++){
                 for(int z=0; z < 100; z+=5){
                     char[] block = new char[10];
                     for(int i=0; i<5; i++){
-                        block[i] = structure1[x][y][z+i];
-                        block[i+5] = structure2[x][y][z+i];
+                        block[i] = (char) structure1[x][y][z+i];
+                        block[i+5] = (char) structure2[x][y][z+i];
                     }
                     blocks.add(new Block(UUID.randomUUID(), String.copyValueOf(block)));
                 }
@@ -92,7 +66,6 @@ public class Ring extends Subscriber{
     
     @Subscribe
     public void receive(RunExperimentFull event){
-        this.generateProtons(ExperimentScope.ESFull);
         this.currentExperiment = new Experiment();
         this.energy = event.initialEnergy;
         this.activateMagneticField();
@@ -100,15 +73,12 @@ public class Ring extends Subscriber{
             this.energy = Math.min(this.energy + 25000, 300000);
         }
         this.releaseProton();
-        for(int i=0; i < this.protonTraps.length; i+=2){
-            this.collide(this.protonTraps[i].protons.pop(), this.protonTraps[i+1].protons.pop());
-            event.controlCenter.startAnalyse(this.currentExperiment);
-        }
+        this.collide(this.protonTraps[0].protons.pop(), this.protonTraps[1].protons.pop());
+        this.detector.addExperiment(this.currentExperiment);
     }
     
     @Subscribe
     public void receive(RunExperimentPartial event){
-        this.generateProtons(event.scope);
         this.currentExperiment = new Experiment();
         this.energy = event.initialEnergy;
         this.activateMagneticField();
@@ -116,13 +86,7 @@ public class Ring extends Subscriber{
             this.energy = Math.min(this.energy + 25000, 300000);
         }
         this.releaseProton();
-        try{
-            for(int i=0; i < this.protonTraps.length; i+=2){
-                this.collide(this.protonTraps[i].protons.pop(), this.protonTraps[i+1].protons.pop());
-                event.controlCenter.startAnalyse(this.currentExperiment);
-            }
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
+        this.collide(this.protonTraps[0].protons.pop(), this.protonTraps[1].protons.pop());
+        this.detector.addExperiment(this.currentExperiment);
     }
 }
