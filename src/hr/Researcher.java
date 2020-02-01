fa3afcd6e1660d6d71e151459aa49598465a5051
Kenny16372 <44952455+Kenny16372@users.noreply.github.com>
2020-02-01 16:29:23 +0100

    Now outputs the results of an Experiment

--- src/hr/Researcher.java
+++ src/hr/Researcher.java
@@ -15,6 +15,5 @@ public class Researcher extends Employee {
 
     public void updateExperiment(Detector detector){
         this.experiment = detector.getCurrentExperiment();
-        System.out.println(experiment.getUUID() + ":" + experiment.getIsHiggsBosonFound());
     }
 }
